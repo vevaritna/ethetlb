2d39901e6267d4374317cb94b64133cdb2bcbfdd
vevaritna <64547107+vevaritna@users.noreply.github.com>
2020-05-06 20:02:56 +0300

    Add files via upload

---
+++ 224/js/translater.js
@@ -0,0 +1,167 @@
+var new_lang = {
+	
+
+  "lt0": "forradalmian új<img alt=\"\" src=\"img/@2x/p1.png\"><span>ketogén módszer!</span>",
+  
+  "lt1": "csak ma!<span>50%-os kedvezmény</span>",
+  
+  "lt2": "új ár:<span class=\"price_main\"></span>",
+  
+  "lt3": "régi ár:<span class=\"price_old\"></span>",
+  
+  "lt4": "Akció végéig maradt:",
+  
+  "lt5": "Válasszon országot:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt6": "Adja meg a nevét:<input name=\"name\" placeholder=\"Kovács Anna\" required=\"\" type=\"text\">",
+  
+  "lt7": "Adja meg a telefonszámát:<input name=\"phone\" placeholder=\"+7 (937) 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt8": "rendelje meg most",
+  
+  "lt9": "rendelje meg most",
+  
+  "lt10": "Biztonságos fogyás<span>ketózis révén</span>",
+  
+  "lt11": "<b>Ketózis során nem glükózból, hanem zsírokból nyerünk energiát, amit a szervezetünkbe szénhidrátok formájában viszünk be.</b>Ha a szénhidrátokból nem jut elég energiához, akkor 7-14 napon belül ketózis állapota lép fel. Ennek eredményeként megkezdődik a zsírok nagy ütemben történő energiává alakítása.",
+  
+  "lt12": "Az emberi szervezet azonban nehezen képes elviselni olyan étrendeket, amelyek a szénhidrátok teljes kizárását írják elő.<br><span style=\"color: #9f116b; font-weight: 700;\">A KETO DIETA segítségével önsanyargatás nélkül indíthatja el a ketózis folyamatát.</span>A komplexum biztosítja a ketontestek kívülről történő bejutását a szervezetbe, ennek révén megszabadulhat a felesleges zsírtól.",
+  
+  "lt13": "A MEGOLDÁS:",
+  
+  "lt14": "KETO",
+  
+  "lt15": "DIETA",
+  
+  "lt16": "keto<span>diet</span>",
+  
+  "lt17": "Az étrend nélkülözhetetlen eleme.",
+  
+  "lt18": "A kapszula beindítja a szervezetben a ketózis folyamatát és a fölösleges zsírt energiává alakítja át.<b>A mindennapos fogyás garantált.</b>",
+  
+  "lt19": "IDEÁLIS KOMBINÁCIÓ<span class=\"big\">KETÓZIS+</span><span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt20": "A ketózis eléréséhez a szervezetnek általában 7-14 napra van szüksége. Ezt akár egyetlen KETO DIET kapszulával 40-50 perce csökkenthetjük. Ez idő alatt megkezdődik a zsírok energiává történő átalakítása és a felhalmozott zsírfelesleg égetése. Használja naponta a<span>KETO DIET készítményt és szabaduljon meg egy hónap alatt akár 10-15 kilótól!</span>",
+  
+  "lt21": "1. szakasz",
+  
+  "lt22": "A ketózis lehetővé teszi a nehezen elérhető zsírok égetését, miközben nem építi le az izomtömeget. A csökkent szénhidrátbevitel garantálja az optimális súly elérését és nem kell attól félnie, hogy újra visszaszedi a leadott kilókat.",
+  
+  "lt23": "2. szakasz ",
+  
+  "lt24": "A Keto Diet beindítja a ketózis folyamatát: felgyorsítja az anyagcserét, csökkenti az étvágyat és támadást indít a zsírfelesleg ellen!",
+  
+  "lt25": "rendelje meg most",
+  
+  "lt26": "<span class=\"big\">NAGY HATÉKONYSÁGÚ</span>ÖSSZETEVŐK<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt27": "Málna keton",
+  
+  "lt28": "Hatásában hasonló az emberi test által termelt ketontestekhez és rendkívüli aktivitással bír.<ul><li><span>A</span>zsírokat energiává alakítja</li><li><span>Felgyorsítja az</span>anyagcserét</li><li><span>Elnyomja az</span>éhségérzetet</li></ul>",
+  
+  "lt29": "Beta sitosterol",
+  
+  "lt30": "Enyhíti a helytelen táplálkozás okozta következményeket<ul><li><span>Helyreállítja a</span>lipid anyagcserét</li><li><span>Megtisztítja az érrendszert</span>az egészségre káros koleszterintől</li><li><span>Formálja az alakot,</span>küzd a cellulitisz ellen</li></ul>",
+  
+  "lt31": "Koenzim Q10",
+  
+  "lt32": "Az elhízást az esetek 78%-ában a koenzim Q10 hiánya okozza<ul><li><span>A koenzim Q10 normalizálja az</span>anyagcserét</li><li><span>Csökkenti a</span>koleszterinszintet</li><li><span>Megakadályozza a</span>túlevést</li></ul>",
+  
+  "lt33": "L-karnitin",
+  
+  "lt34": "A B-vitaminok csoportjába tartozó anyag szintetizálódik az emberi testben <ul><li><span>A zsírokat</span>energiává alakítja át</li><li><span>Csökkenti az ingerlékenységet,</span>javítja a hangulatot</li><li><span>Segít leküzdeni a</span>depressziót</li></ul>",
+  
+  "lt35": "Bromelain",
+  
+  "lt36": "Fehérje és zsírok lebontásáért felelős enzim, amelyet az ananász gyümölcséből és szárából nyernek ki <ul><li><span>Aktívan égeti a</span>vaszkuláris zsírt</li><li><span>Eltávolítja a</span>duzzanatot</li><li><span>Megelőzi a</span>belső szervek elhízását</li></ul>",
+  
+  "lt37": "Természetes szinefrin",
+  
+  "lt38": "Erősen lipolitikus hatású. Blokkolja az alfa-receptorok aktivitását a zsírsejtekben <ul><li><span>Hatékonyan megküzd a „kitartó” felesleggel, beleértve a vaszkuláris zsírt</span></li><li><span>Fokozza a</span>szervezet által feldolgozott kalóriamennyiséget</li><li><span>Javítja a teljesítőképességet</span>és az állóképességet</li></ul>",
+  
+  "lt39": "SZAKÉRTŐI VÉLEMÉNY<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt40": "<span>Dorothy Goodall </span>, amerikai dietológus, táplálkozási szakértő, 30 éves szakmai tapasztalat.",
+  
+  "lt41": "Ketózis során a szervezet a felhalmozott zsírokat zsírsavakká és glicerinné bontja, amelyek később ketontestekké alakulnak át. A folyamat azonban csak akkor indul be, amikor a májban és az izomszövetben felhalmozott glikogén teljesen kiürül,<b>ehhez a szervezetnek általában 7-14 napra van szüksége.</b>",
+  
+  "lt42": "Ahhoz, hogy felgyorsítsuk a folyamatot, és ennek megfelelően hamarabb megkezdődhessen a zsírégetés, a betegeimnek a KETO DIET táplálék-kiegészítőt szoktam ajánlani. A KETO DIET erősíti a szervezetet ketogén diéta közben: tápanyagokkal látja el, elnyomja az éhségérzetet, megszünteti a fáradtságot és a szorongást.<b>A KETO DIET biztosítja az egészséges és természetes fogyást. Nem elhanyagolandó tény, hogy mindezt anélkül teszi, hogy fogyás után a bőr megnyúljon, ezáltal jótékony hatással van a testre.</b>",
+  
+  "lt43": "A KETO DIET legfőbb tulajdonsága, hogy hamarabb beindítja a ketózis folyamatát, segíti a zsírok energiává történő átalakulását.",
+  
+  "lt44": "rendelje meg most",
+  
+  "lt45": "Csatlakozzon ön is<b>ahhoz a sok ezer emberhez,</b>akik már kipróbálták a<span class=\"h3\">keto-t<span>diet</span></span>",
+  
+  "lt46": "Visszajelzések",
+  
+  "lt47": "Előző",
+  
+  "lt50": "”Amióta az eszemet tudom, mindig is diétáztam. Éhezés, alvászavarok és a bőröm sem éppen ideális – nos, ez mind rám jellemző. Néha le tudtam adni pár kilót, de nem kellett hozzá sok, hogy újra visszaszedjem. A KETO DIET-et egy dietológus ajánlotta. Szinte azonnal éreztem az eredményt. A mérleg minden héten 3 kilóval kevesebbet mutatott. Számomra ez valódi csoda volt. Nagyon jó lett a közérzetem, azóta nem éhezem, és legalább 7 órát alszom”.",
+  
+  "lt51": "K. Nóra, 27 éves",
+  
+  "lt52": "”Számomra a KETO DIET valódi megmentés volt. Korábban álmodni se mertem arról, hogy bármit megehessek és emellett fogyjak is. Az igazság az, hogy mindig is szénhidrátban gazdag ételeket ettem. Ez odáig vezetett, hogy 29 éves koromra nagyon elhíztam, 100 fölött voltam. Szóval jó, hogy egy kicsit lecsökkentettem a szénhidrát bevitelt, mert a súly így kezdett lemenni. De sajnos nagyon szeretem a gyorséttermi kaját, nem tudtam meglenni nélküle. Aztán hallottam a KETO DIET-ről. Elkezdtem szedni, és azóta sokkal gyorsabban égetem a fölöst, ráadásul nem kell lemondanom azokról az ételekről, amiket szeretek. 4 hónap alatt körülbelül 25 kilót leadtam. A KETO DIET baromi jó! Mindenkinek ajánlom!”",
+  
+  "lt53": "F. Dóra, 49 éves",
+  
+  "lt54": "Milyen keveset tudunk a saját testünkről. Nem is gondoltam, mennyire bonyolult folyamatok játszódnak le bennünk, amíg el nem mentem egy táplálkozási szakértőhöz, aki beszélt nekem a ketózisról. Nem szeretem a diétát, és ami azt illeti, sportolni sem igazán szeretek, mivel lusta vagyok rá. Ezért olyat kerestem, ami könnyen és gyorsan megy. A KETO DIET-ről annyit mondhatok: meg kell venni! Ezzel a szerrel rekordot döntöttem: 15 kilót sikerült leadnom és ezzel elértem a tökéletes alakot.",
+  
+  "lt55": "R. Anna, 34 éves",
+  
+  "lt62": "Következő",
+  
+  "lt63": "Itt az ideje,<b>hogy megszabaduljon a komplexusoktól</b>",
+  
+  "lt64": "forradalmian új<img alt=\"\" src=\"img/@2x/p1.png\"><span>ketogén módszer!</span>",
+  
+  "lt65": "csak ma!<span>50%-os kedvezmény</span>",
+  
+  "lt66": "új ár:<span class=\"price_main\"></span>",
+  
+  "lt67": "régi ár:<span class=\"price_old\"></span>",
+  
+  "lt68": "Akció végéig maradt:",
+  
+  "lt69": "Válasszon országot:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt70": "Adja meg a nevét:<input name=\"name\" placeholder=\"Kovács Anna\" required=\"\" type=\"text\">",
+  
+  "lt71": "Adja meg a telefonszámát:<input name=\"phone\" placeholder=\"+7 (937) 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt72": "rendelje meg most",
+  
+  "lt73": "Hogyan dolgozunk?",
+  
+  "lt74": "RENDELÉS LEADÁSA",
+  
+  "lt75": "A rendelés leadása után munkatársunk azonnal felveszi önnel a kapcsolatot, és<br><b>megerősíti a rendelést</b>",
+  
+  "lt76": "KISZÁLLÍTÁS",
+  
+  "lt77": " A rendelés leadása után munkatársunk azonnal felveszi önnel a kapcsolatot, és<br><b> megerősíti a rendelést</b>",
+  
+  "lt78": "FIZETÉS",
+  
+  "lt79": "<b>Itt nem kell előre fizetni!</b>A megrendelés kifizetése szigorúan átvételkor történik.",
+  
+  "lt80": "Garancia",
+  
+  "lt81": "Internetes<br>áruházunkban vásárolva biztos lehet afelől, hogy termékeink a legmagasabb minőséget nyújtják"
+  
+  
+  
+
+
+	
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
